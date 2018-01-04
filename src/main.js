f029d5653be26a990402b09df59b305227ad3716
Inesa <inesatat@ukr.net>
2018-01-04 22:43:54 +0200

    first commit

---
+++ src/main.js
@@ -0,0 +1,14 @@
+var name = 'Inesa';
+var salary = 2000;
+var age = 24;
+var bool = false;
+
+console.log(name, salary, age, bool);
+
+var obj = {
+    person: {
+        status: 'easycode student'
+    }
+};
+console.log(obj.person)
+console.log(obj.person.status)
